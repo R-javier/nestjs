26d56a340988aab82e851104d17a4536fb58463d
Javier Rodriguez <javiplus.-@live.com.ar>
2025-12-29 17:47:55 -0300

    Agregado logger.middleware.ts y configuracion en AppModule con forRoutes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
-
-import { Module } from '@nestjs/common';
+// Importamos NestModule y MiddlewareConsumer para poder registrar middlewares.
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+//Nuestro middleware personalizado para logs.
+import { LoggerMiddleware } from './cat/logger.middleware';
 import { CatModule } from './cat/cat.module';
+import { CatController } from './cat/cat.controller';
 
 @Module({
   imports: [CatModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {// El middleware se configura acá, no dentro del decorador @Module().
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    //Registramos el middleware que queremos aplicar
+    .apply(LoggerMiddleware)
+    //Definimos a que rutas o controladores se aplica(en este caso, todas 
+    //las rutas 'cats')
+    .forRoutes(CatController);
+  }
+}
+
+
+// EJEMPLO DE .forRoutes
+// 1) Middleware aplicado a todas las rutas que comienzan con /cats.
+// .forRoutes('cats'); ~ Todas las rutas que empiezan con /cats
+//OTRAS OPCIONES
+// 2) Middleware restringido a un método HTTP específico en este caso GET.
+// .forRoutes({ path: 'cats', method: RequestMethod.GET }); ~ solo GET /cats
+// 3) Middleware aplicado directamente a un controlador -
+// Se aplique a todas las rutas que define CatsController, independientemente del path base.
+// .forRoutes(CatController); ~ Todas las rutas del controlador
